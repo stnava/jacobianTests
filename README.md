73c772b922e65aa39e0b69c1333017f9f13f5f43
brian avants <stnava@gmail.com>
2018-11-15 13:54:37 -0500

    DOC: more info on the jacobian and reference calculations.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # jacobianTests
 very basic test of jacobian
 should get a value around 2.14
+
+the rcalc.R compares the following values:
+
+* sum of the jacobian computed in the fixed space within the mask defining the boundaries of the circle1 image.  this should give the estimated volume because the spacing of the images is 1x1mm.
+
+* 
+
+
+From the ants pdf documentation at [https://github.com/stnava/ANTsDoc](https://github.com/stnava/ANTsDoc):
+
+Figure 7: Digital invertibility presents some limitations. Here, we see that invertibility is not exact but is gained only by interpolation. Thus, in three-dimensional scenarios in particular, there are fundamental limits to the degree of invertibility that may be achieved. The second and third voxels – from left – in image A undergo an expansion by a factor of 2. That is, under the mapping, 2 voxels are mapped to 4. This gives the definition of the Jacobian – computed by ANTsJacobian – which is a unitless measure defined by the ratio of volumes. Thus, J (x) = V (phi(x))/V (x) where V represents the volume operation and x, here, may be a small object. Thus, if phi – the mapping – causes expansion, then J (x) > 1.  In this example phi( x ) is the warp defined in the fixed image space e.g.  myOutputWarp.nii.gz
+
+See also Figure 21 and 22.
--- rcalc.R
+++ rcalc.R
@@ -3,6 +3,19 @@ library(ANTsR)
 j=antsImageRead("/tmp/circAjacobian.nii.gz")
 circ = antsImageRead( "circ1.nii.gz" )
 circ2 = antsImageRead( "circ2.nii.gz" )
-print(( sum( j[ circ == 1 ] / sum( circ == 1 ) ) ) )
-print( sum(  sum( circ2 == 1 )/ sum( circ == 1 ) ) )
+print( paste("Ratio Jacobian: " , ( sum( j[ circ == 1 ] / sum( circ == 1 ) ) )  ))
+print( paste("Ratio Voxel Counts: ", sum(  sum( circ2 == 1 )/ sum( circ == 1 ) ) ) )
+print( paste("Volume by jacobian", prod( antsGetSpacing( circ ) ) * sum( j[circ==1] ) ))
+print( paste("Volume by counting voxels", prod( antsGetSpacing( circ ) )*sum(circ2==1)))
 
+print("Resample the images and do the same test ( using registration in ANTsR):")
+circ2r = resampleImage( circ2, 1.5 )
+circ1r = resampleImage( circ, 0.5 )
+myreg = antsRegistration( circ1r, circ2r, 'SyNOnly',
+  synMetric = 'meansquares',
+  regIterations = c( 100,100, 10 )    )
+antsrjac = createJacobianDeterminantImage( circ1r, myreg$fwdtransforms[1] )
+print( paste("Volume by jacobian", prod( antsGetSpacing( circ1r ) ) * sum( antsrjac[circ1r==1] ) ))
+print( paste("Volume by counting voxels", prod( antsGetSpacing( circ2r ) )*sum(circ2r==1)))
+
+print("In conclusion, the sum of the jacobian within the object in the template space -- multiplied by the product of the image spacing -- approximates the volume of the object within the moving image.  Assuming you have only applied the SyN transformation by itself.")
